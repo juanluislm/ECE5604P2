edeeeff233b9dd1c4ab59d6934fe19b95015d5c6
jorgeeduardo <edu338@vt.edu>
2016-03-22 10:54:44 -0400

    Tests on Tuesday March 22
    File update

--- Data_analysis.m
+++ Data_analysis.m
@@ -2,19 +2,30 @@ clear
 close all
 %% Load data
 load Tx_sinusoids % = sines(Nf, N_samples), cosines(Nf, N_samples)
-load Rx_sines
-load Rx_cosines
-%%
-N_RxSines = length(Rx_sines);
-N_RxCos = length(Rx_cosines);
+load Rx_sines_cable
+load Rx_cosines_cable
+%% Extract some parameters
+N_RxSines = length(Rx_sines); % Number of received sine samples
+N_RxCos = length(Rx_cosines); % Number of received cosine samples
+Nf = length(sines(:,1)); % Number of sinusoids
 
-
-
-
-return
-Tx_SinesConcat = reshape(sines, 1,numel(sines));
-[Xs,w] = freqz(Tx_SinesConcat,1,2^22,1);
-[Ys,w] = freqz(Rx_sines,1,2^22,1);
-figure;
-plot(w,db(Xs)); hold on;
-plot(w,db(Ys),'r'); legend('Tx','Rx')
+%% Try to separate the sinusoids
+thresh = 0.5e-3;
+buff = ones(1,500);
+Rx_sines_mat = zeros(Nf,round(N_RxSines/Nf)+100e3);
+% For the sines:
+freq_idx_counter = 1;
+sample_idx_counter = 1;
+for i = 1:1:N_RxSines
+    Rx_sines_mat(freq_idx_counter,sample_idx_counter) = Rx_sines(i);
+    sample_idx_counter = sample_idx_counter+1;
+    
+    
+    buff = [buff(2:end),Rx_sines(i)];
+    if mean(abs(buff)) < 5e-4
+        freq_idx_counter = freq_idx_counter+1;
+        buff = ones(1,1500);
+    end
+    
+end
+ 
--- Rx_AudioTes_Mic.m
+++ Rx_AudioTes_Mic.m
@@ -1,7 +1,7 @@
 clear 
 close all
 %%
-Fs = 44100;
+Fs = 44.1e3;
 r = audiorecorder(Fs, 16, 1);
 record(r);     % speak into microphone...
 
