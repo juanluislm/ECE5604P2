aa082dc0bc0f6c64a2a592d72922ad75d9db0445
Wnstjd <choijs89@vt.edu>
2016-04-11 11:50:02 -0400

    Added files via upload
    fixed filtered signal add slicing point search capability

--- Rx_filter_not_merged.m
+++ Rx_filter_not_merged.m
@@ -2,10 +2,12 @@ clc;
 close all;
 load Rx_not_merged
 load Rx_merged
+load 6syms_test
 
-rxs_not_merged = Rx_not_merged'.*(exp(-2*pi*1i*4500/44100)*[1:length(Rx_not_merged)]);
+rxs_not_merged = Rx_not_merged'.*exp(-2*pi*1i*4500/44100*[1:length(Rx_not_merged)]);
 %txs=real(txs);
-plot(rxs_not_merged)
+% figure (1)
+% plot(rxs_not_merged)
 
 k = 28; % samples per symbol
 m = 8; %spread
@@ -20,16 +22,24 @@ ol=wl/2; %overlap
 b_ord = 2000;
 b = firpm(b_ord,[0 0.0317 0.0363 0.5]*2,[1 1 0 0 ], [10 1]);
 b_rxs_not_merged = filter(b,1,[rxs_not_merged zeros(1,10)]);
-freqz(b_rxs_not_merged,1,2^18,44.1e3)
-
+%freqz(b_rxs_not_merged,1,2^18,44.1e3)
 b_rxs = filter(shape,1,b_rxs_not_merged);
-freqz(b_rxs,1,2^18,44.1e3)
+%freqz(b_rxs,1,2^18,44.1e3)
 
+%id_x = 1000:20000;
+% figure(2)
+% subplot(2,1,1);
+% plot(real(b_rxs(id_x)));
+% subplot(2,1,2);
+% plot(imag(b_rxs(id_x)));
 
-id_x = 1000:20000;
+% Find Slicing point
+slice_ids = (m*k+(b_ord/2)+1):k:length(b_rxs) ;%(2*m*k+1):NSPS:length(frx1)
+slice_ids1 = slice_ids(1:20000) ; % limit to comparison with number of symbols sent
+b_rxs1 = b_rxs(slice_ids1) ;
+sym1_est = sign(real(b_rxs1)) + 1i*sign(imag(b_rxs1)) ;
+err_ids = find(sym1_est~=syms1) ;
 
-figure(1)
-subplot(2,1,1);
-plot(real(b_rxs(id_x)));
-subplot(2,1,2);
-plot(imag(b_rxs(id_x)));
+% BER
+BER = ((sum(abs(sign(real(b_rxs1(err_ids)))-real(syms1(err_ids))) ...
+   + abs(sign(imag(b_rxs1(err_ids)))-imag(syms1(err_ids)))))/2)/(2*20000)
